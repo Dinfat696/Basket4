980adf7e70e110563dfb57eea854c12edd4e83d2
dinarfathrahmanov <dinfat@mail.ru>
2023-06-10 11:28:08 +0300

    first commit

---
+++ src/main/java/com/basket4/basket4/Basket4Application.java
@@ -0,0 +1,13 @@
+package com.basket4.basket4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basket4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Basket4Application.class, args);
+    }
+
+}
